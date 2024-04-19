70df7250493e92f3bc5e74bdee9416c597789d22
viktor <araujo>
2024-04-18 22:31:10 -0300

    feat: scopes criados

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  scope :max_price, ->(price) { where('price <= ?', price) if price.present? }
+  scope :min_price, ->(price) { where('price >= ?', price) if price.present? }
+
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [50, 50]
     attachable.variant :medium, resize_to_limit: [250, 250]
