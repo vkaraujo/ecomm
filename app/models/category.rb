edc80219b701630f1d9783b9e0e3a78da400331a
viktor <araujo>
2024-04-19 20:48:39 -0300

    validation added to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [50, 50]
     attachable.variant :large, resize_and_pad: [512, 512]
